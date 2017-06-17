a2870316f7596da0e544a535a76198e1fff146ca
jkjh0819 <jkjh0819@naver.com>
2017-06-17 16:51:58 +0900

    finish cow positioned

---
+++ PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/SimpleScene.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/SimpleScene.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/WaveFrontOBJ.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/WaveFrontOBJ.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/intersectionTest.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/intersectionTest.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/interval.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/interval.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/matrix4.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/matrix4.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/quater.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/quater.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/vector3.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/vector3.cpp.o"
+  "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/math/vector4.cpp" "/Users/jihyejegal/git/ComputerGraphics/PA2_2017_skeleton/CMakeFiles/OgreFltk.dir/math/vector4.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
